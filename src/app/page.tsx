f8c68a679dd6112d0e8e2994604b2f446a45c588
nikitanosov1 <nikinosov2002@mail.ru>
2023-10-18 22:35:18 +0400

    fix pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import styles from "./page.module.css";
-import { FormControlLabel, FormGroup, Switch, Typography } from "@mui/material";
+import {
+  FormControlLabel,
+  FormGroup,
+  Stack,
+  Switch,
+  Typography,
+} from "@mui/material";
 import { darkTheme, lightTheme } from "@/config/theme";
 import { ThemeProvider } from "@emotion/react";
-import { Home } from "./home/page";
 import { selectIsDarkTheme } from "@/redux/features/theme/selector";
 import { useDispatch, useSelector } from "react-redux";
 import { themeActions } from "@/redux/features/theme";
+import { Graph } from "@/components/Graph/Graph";
+import { GraphSettings } from "@/components/GraphSettings/GraphSettings";
 
 export default function App() {
   const isDarkTheme = useSelector(selectIsDarkTheme);
@@ -15,7 +22,22 @@ export default function App() {
 
   return (
     <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
-      <Home />
+      <Stack
+        sx={(theme) => ({
+          backgroundColor: theme.palette.background.paper,
+        })}
+        className={styles.main}
+        flexDirection="row"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Stack flexDirection="row" justifyContent="space-between" gap={2}>
+          <Graph />
+          <Stack pt="6px">
+            <GraphSettings />
+          </Stack>
+        </Stack>
+      </Stack>
       <div
         style={{
           position: "absolute",
