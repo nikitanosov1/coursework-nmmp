02eea78d95d58225f91ce82245c331ec4f4c7d25
nikitanosov1 <nikinosov2002@mail.ru>
2023-10-24 22:25:19 +0400

    fix config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/coursework-nmmp",
+  assetPrefix: "/coursework-nmmp/",
+  output: "export", // только для того, чтобы превратить NextJS проект в статичный для деплоя на gh-pages
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
